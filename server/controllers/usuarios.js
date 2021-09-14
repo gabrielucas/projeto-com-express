ebf0a3346be940246aeee850ac45be72518a700d
Lucas Viana <lucaasviana280@gmail.com>
2021-09-13 21:59:32 -0300

    feature: adicionando página de usuário com parâmetro na rota

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -5,7 +5,19 @@ const controller = {
     res.render('usuarios', {
       titulo: 'Usuários',
       subtitulo: 'Você está na página de usuários',
-      usuarios: usuariosPlaceholder
+      usuarios: usuariosPlaceholder,
+      bannerTopo: '/images/banner-topo-usuarios-1564x472.png'
+    });
+  },
+  show: function (req, res, next) {
+
+    const {id} = req.params
+
+    res.render('usuario', {
+      titulo: 'Usuário',
+      subtitulo: `Você está na página do usuário ${id}`,
+      usuario: usuariosPlaceholder[id],
+      bannerTopo: '/images/banner-topo-usuario-1564x472.png'
     });
   }
 }
